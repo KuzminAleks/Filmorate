bb8ed868ab247ac6289e21dc64db0c9eab4e8623
ofermad <redturner@mail.ru>
2024-11-01 16:50:42 +0300

    Добавить БД

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -2,95 +2,116 @@ package ru.yandex.practicum.filmorate.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.dal.dao.UserDbStorage;
+import ru.yandex.practicum.filmorate.dal.dto.UserDto;
+import ru.yandex.practicum.filmorate.dal.mapper.UserMapper;
+import ru.yandex.practicum.filmorate.exception.InternalServerException;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
-import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
-import java.util.Collection;
-import java.util.Set;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
+
+import static ru.yandex.practicum.filmorate.dal.mapper.UserMapper.mapToUserDto;
 
 @Service
 public class UserService {
-    private final UserStorage userStorage;
-
     private final Logger log = LoggerFactory.getLogger(UserService.class);
 
-    @Autowired
-    public UserService(InMemoryUserStorage userStorage) {
-        this.userStorage = userStorage;
+    private final UserDbStorage userDbStorage;
+
+    public UserService(UserDbStorage userDbStorage) {
+        this.userDbStorage = userDbStorage;
     }
 
-    public User addUser(User user) {
-        return userStorage.addUser(user);
-    }
-
-    public User updateUser(User user) {
-        return userStorage.updateUser(user);
-    }
-
-    public Collection<User> getAllUsers() {
-        return userStorage.getAllUsers();
-    }
-
-    public User addFriend(Integer userId, Integer userFriendId) {
-        User user = userStorage.getUserById(userId);
-        User userFriend = userStorage.getUserById(userFriendId);
-
-        log.debug("До добавления в друзья: {}", user);
-        log.debug("До добавления в друзья: {}", userFriend);
-
-        user.getFriends().add(userFriendId);
-        userFriend.getFriends().add(userId);
-
-        log.debug("После добавления в друзья: {}", user);
-        log.debug("После добавления в друзья: {}", userFriend);
-
-        return user;
-    }
-
-    public Collection<User> getUserFriends(Integer userId) {
-        log.trace("Пользователь: {}", userStorage.getUserById(userId));
-        log.trace("Друзья пользователя: {}", userStorage.getUserById(userId).getFriends());
-
-        return userStorage.getAllUsers().stream()
-                .filter(user -> userStorage.getUserById(userId).getFriends().contains(user.getId()))
+    public List<UserDto> getAllUsers() {
+        return userDbStorage.getAllUsers().stream()
+                .map(UserMapper::mapToUserDto)
                 .collect(Collectors.toList());
     }
 
-    public User deleteFriend(Integer userId, Integer userFriendId) {
-        User user = userStorage.getUserById(userId);
-        User userFriend = userStorage.getUserById(userFriendId);
+    public UserDto getUserById(Integer userId) {
+        Optional<User> user = userDbStorage.getUserById(userId);
 
-        log.debug("Пользователь 1 до удаления из друзей: {}", user);
-        log.debug("Пользователь 2 до удаления из друзей: {}", userFriend);
-
-        user.getFriends().remove(userFriendId);
-        userFriend.getFriends().remove(userId);
-
-        log.debug("Пользователь 1 после удаления из друзей: {}", user);
-        log.debug("Пользователь 2 после удаления из друзей: {}", userFriend);
-
-        return user;
+        return user.map(UserMapper::mapToUserDto).orElseThrow(() -> new NotFoundException("Пользователь не найден."));
     }
 
-    public Collection<User> getMutualFriends(Integer user1Id, Integer user2Id) {
-        User user1 = userStorage.getUserById(user1Id);
-        User user2 = userStorage.getUserById(user2Id);
+    public UserDto addUser(User user) {
+        log.debug(user.toString());
 
-        log.debug("Пользователь 1: {}", user1);
-        log.debug("Пользователь 2: {}", user2);
+        if (isValidated(user)) {
+            log.debug(user.toString());
 
-        Set<Integer> mutualFriends = user1.getFriends().stream()
-                .filter(friend -> user2.getFriends().contains(friend))
-                .collect(Collectors.toSet());
+            return mapToUserDto(userDbStorage.addUser(user));
+        }
 
-        log.debug("Id общих друзей пользователей 1 и 2: {}", mutualFriends);
+        throw new InternalServerException("Пользователь не добавлен, что-то пошло не так.");
+    }
 
-        return userStorage.getAllUsers().stream()
-                .filter(users -> mutualFriends.remove(users.getId()))
+    public UserDto updateUser(User user) {
+        log.debug(user.toString());
+
+        if (isValidated(user)) {
+            log.debug(user.toString());
+
+            return mapToUserDto(userDbStorage.updateUser(user));
+        }
+
+        throw new InternalServerException("Пользователь не обновлен, что-то пошло не так.");
+    }
+
+    public boolean deleteUser(Integer userId) {
+        return userDbStorage.deleteUser(userId);
+    }
+
+    public UserDto addFriend(Integer userId, Integer friendId) {
+        Optional<User> user = userDbStorage.addFriend(userId, friendId);
+
+        log.info(user.toString());
+
+        return user.map(UserMapper::mapToUserDto).orElse(null);
+    }
+
+    public UserDto removeFriend(Integer userId, Integer friendId) {
+        Optional<User> user = userDbStorage.removeFriend(userId, friendId);
+
+        return user.map(UserMapper::mapToUserDto).orElse(null);
+    }
+
+    public List<UserDto> getUserFriends(Integer userId) {
+        return userDbStorage.getUserFriends(userId).stream()
+                .map(UserMapper::mapToUserDto)
                 .collect(Collectors.toList());
+    }
+
+    public List<UserDto> getMutualFriends(Integer user1Id, Integer user2Id) {
+        List<User> user1Friends = userDbStorage.getUserFriends(user1Id);
+        List<User> user2Friends = userDbStorage.getUserFriends(user2Id);
+
+        return user2Friends.stream()
+                .filter(user1Friends::contains)
+                .map(UserMapper::mapToUserDto)
+                .collect(Collectors.toList());
+    }
+
+    private boolean isValidated(User user) {
+        if (user.getEmail() == null || user.getEmail().isBlank()
+                || !user.getEmail().contains("@")) {
+            throw new ValidationException("Неверная почта!");
+        }
+
+        if (user.getLogin() == null || user.getLogin().isBlank() || user.getLogin().contains(" ")) {
+            throw new ValidationException("Логин не может быть пустым и содержать пробелы.");
+        }
+
+        if (user.getBirthday().isAfter(LocalDate.now())) {
+            throw new ValidationException("Дата рождения не может быть в будущем!");
+        }
+
+        return true;
     }
 }
